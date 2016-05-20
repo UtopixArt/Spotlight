dd8848ea867eb05bded37b963ef768dc82225c5d
UtopixArt <utopiqdream@gmail.com>
2016-05-20 06:19:56 +0200

    fix css and fitimage

--- spotlight.js
+++ spotlight.js
@@ -65,17 +65,17 @@
 
 		Spotlight.prototype.cacheDiv = function(){
 		
-		this.next = '<img id = "rightIcon" src="Spotlight/icons/next.png">'
-		this.prev = '<img id = "leftIcon" src="Spotlight/icons/prev.png">'
-		this.close = '<img id = "closeIcon" src="Spotlight/icons/close.png">'
-
-		this.rightButton = '<div id = "rightButton">'+this.next+' </div>'
-		this.leftButton = '<div id = "leftButton">'+this.prev+'</div>'
-		this.closeButton = '<div id = "closeButton">'+this.close+'</div>'
+		this.leftButton = '<div id = "leftButton"></div>'
+		this.rightButton = '<div id = "rightButton"></div>'
+		this.closeButton = '<div id = "closeButton"></div>'
 		this.image = '<img id ="show">'
 
-		this.container = '<div class = "container">'+this.leftButton+this.image+this.rightButton+this.closeButton+'</div>';
-		this.spotlight = '<div id = "spotlight">'+this.container+'</div>';
+		this.navcontainer = '<div id = "navcontainer">'+this.leftButton+this.rightButton+'</div>'
+		
+		this.imgcontainer ='<div id = "imgcontainer">'+this.navcontainer+this.image+'</div>'
+		
+		this.container = '<div id = "container">'+this.imgcontainer+this.closeButton+'</div>'
+		this.spotlight = '<div id = "spotlight">'+this.container+'</div>'
 
 		$(this.spotlight).appendTo($('body'));
 		
@@ -83,16 +83,10 @@
 	};
 
 	Spotlight.prototype.update = function(){
-		
-		$('#rightIcon').hide();
-		$('#rightButton').mouseover(function(){$('#rightIcon').fadeIn('fast')});
-		$('#rightButton').mouseout(function(){$('#rightIcon').fadeOut('fast')});
-		$('#leftIcon').hide();
-		$('#leftButton').mouseover(function(){$('#leftIcon').fadeIn('fast')});
-		$('#leftButton').mouseout(function(){$('#leftIcon').fadeOut('fast')});			
+				
 		this.nav(); //call nav logic
 		this.end(); //close button method "jquery"
-
+		this.sizeContainer(); // call method sizeContainer for fill container on screen
 	};
 
 	Spotlight.prototype.nav = function(){
@@ -132,22 +126,29 @@
 		};	
 	};
 
+	Spotlight.prototype.sizeContainer = function() {
+	    var $spotlight = $('#spotlight');
+	    $spotlight.width($(document).width());
+	    $spotlight.height($(document).height());    
+	};
+
 	Spotlight.prototype.showImage = function(numberImage) {
 	    var self = this;// pass through closure
 	    var $image = $('#show'); //stock l'id $('#Show')
 	    var preloader = new Image();// use object image for grab image size
-	   	  
-	   	var windowWidth = $(window).width() , //stock window dimention
-	   		windowHeight  = $(window).height(),
+		   	  
+	   	var windowWidth = $('#imgcontainer').width() , //stock window dimention
+	   		windowHeight  = $('#imgcontainer').height(),
 	   		imageWidth,
 	   		imageHeight,
 	   		ratio,
 	   		wRatio,
 	   		hRatio,
-	   		size = 0.7;    
+	   		size = 1;    
+
 	    preloader.onload = function(){ //onload function
 	    	$image.attr('src',self.album.linksList[numberImage].link); // change content on $('#show'), display
-		
+			
 			//fit image on screen method
 			$image.width(preloader.width * size)
 			$image.height(preloader.height * size)
@@ -158,31 +159,50 @@
 		 	
 		 	if(imageWidth > windowWidth || imageHeight > windowHeight){
 		 		if(preloader.width>windowWidth){
-		 			wRatio = windowWidth / preloader.width
-		 			$image.height((imageHeight * wRatio) * size);
-		 			$image.width(windowWidth * size)
+		 			
+		 			if(preloader.width > windowWidth && preloader.height > windowHeight ){
+		 				ratio = windowHeight / preloader.height
+		 				$image.height(windowHeight*size);
+		 				$image.width((imageWidth*ratio)*size);
+		 				console.log("image trop haut et trop large")
+		 				
+		 				if($image.width()>windowWidth){
+		 					ratio = windowWidth / preloader.width
+		 					$image.height((imageHeight*ratio)*size);
+		 					$image.width(windowWidth*size);
+		 					console.log("image encore trop large")
+		 				}
+		 				
+
+		 			}else{
+		 				wRatio = windowWidth / preloader.width
+		 				$image.height((imageHeight * wRatio) * size);
+		 				$image.width(windowWidth * size)
+		 				console.log("image trop large")
+		 			}
+
 		 		}else{
-		 			hRatio = windowHeight / preloader.height
-		 			$image.height(windowHeight * size);
-		 			$image.width((imageWidth * hRatio) * size)
+
+		 			
+	 				hRatio = windowHeight / preloader.height
+	 				$image.height(windowHeight * size);
+	 				$image.width((imageWidth * hRatio) * size)
+	 				console.log("image trop haute")
+		 			
 		 		}
 		 	}
 		}
-		this.sizeContainer(); // call method sizeContainer for fill container on screen
+		
 		preloader.src = this.album.linksList[numberImage].link; //
 		$image.fadeIn('slow','swing');
+		
 		$('#rightButton').fadeIn('slow','swing');
 		$('#leftButton').fadeIn('slow','swing');
 		$('#closeButton').fadeIn('slow','swing'); 	
 		
 	};
-
-	Spotlight.prototype.sizeContainer = function() {
-	    var $spotlight = $('#spotlight');
-	    $spotlight.width($(document).width());
-	    $spotlight.height($(document).height());    
-	};
-
+	
+	
 	Spotlight.prototype.end = function(){
 		var $spotlight = $('#spotlight')
 		
