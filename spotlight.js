d86cea9b48269844c93f2ecd42d9277b01a07447
UtopixArt <utopiqdream@gmail.com>
2016-06-27 00:03:17 +0200

    add noscroll option and center vertical spotlight

--- spotlight.js
+++ spotlight.js
@@ -6,14 +6,14 @@
 						title : [],
 						numberImage : 0 // current image count
 						};
-		
+
 		console.log(this.album);
-		this.init() 
-		
+		this.init()
+
 	};
-	
-	Spotlight.prototype.init = function() {	
-		
+
+	Spotlight.prototype.init = function() {
+
 		this.run();
 		this.cacheDiv();
 		this.update();
@@ -22,9 +22,9 @@
 
 	Spotlight.prototype.run = function() {
 		var self = this // pass through closure
-		
+
 		$('body').on('click', 'a[data-spotLight]', function(event) { //event click on data-spotLight value
-	    	
+
     		self.buildAlbum($(event.currentTarget));
 	    	$('#overlay').fadeIn('slow');
 	    	$('#spotlight').fadeIn('slow');
@@ -37,23 +37,23 @@
 
 	Spotlight.prototype.buildAlbum = function($link){
 		var self = this // pass through closure
-		
+
 		this.album.linksList = []; //reset value of the spotLight
 		this.album.titleList = [];
 		this.album.numberImage = 0;
-		// method push album in the array with object data	
+		// method push album in the array with object data
 		function addToAlbum($link){
 			self.album.titleList.push({
-			title : $link.attr('title'),	
+			title : $link.attr('title'),
 			});
 			self.album.linksList.push({ // push in the ablum.linksList array
 	        link : $link.attr('href'),
 	    	});
-		};	
-		
+		};
+
 		var $links, //cache linksList
 		dataSpotLightValue = $link.attr('data-spotLight');//get attribute from data-spotLight
-		
+
 		//loop storing
 		if (dataSpotLightValue) { // === dataSpotLightValue return true
 			$links = $($link.prop('tagName') + '[data-spotLight="' + dataSpotLightValue + '"]');// assign to links
@@ -66,11 +66,11 @@
 		        	this.album.numberImage = i;
 		      	}
 		    }
-		}	
+		}
 	};
 
 		Spotlight.prototype.cacheDiv = function(){
-		
+
 		this.leftButton = '<div id = "leftButton"></div>';
 		this.rightButton = '<div id = "rightButton"></div>';
 		this.closeButton = '<div id = "closeButton"></div>';
@@ -80,9 +80,9 @@
 		this.image = '<img id ="show">';
 		this.overlay ='<div id ="overlay"></div>';
 		this.navcontainer = '<div id = "navcontainer">'+this.leftButton+this.rightButton+'</div>';
-		
+
 		this.imgcontainer ='<div id = "imgcontainer">'+this.navcontainer+this.image+'</div>';
-		
+
 		this.container = '<div id = "container">'+this.imgcontainer+this.labelData+'</div>';
 		this.spotlight = this.overlay+'<div id = "spotlight">'+this.container+'</div>';
 
@@ -93,7 +93,7 @@
 	};
 
 	Spotlight.prototype.update = function(){
-		this.keyOn = false;		
+		this.keyOn = false;
 		this.click(); //call nav logic
 		this.keyboard();
 		this.responsive();
@@ -103,16 +103,29 @@
 	Spotlight.prototype.sizeContainer = function(){
 	    var $overlay = $('#overlay');
 	    $overlay.width($(document).width());
-	    $overlay.height($(document).height()); 
-
+	    $overlay.height($(document).height());
 	};
+
+	Spotlight.prototype.scroll = function(){
+		var top  = $(window).scrollTop(),
+				left = $(window).scrollLeft();
+
+
+    $('body').addClass('noscroll');
+
+
+		$('#container').css({
+		 top: top + 'px',
+		 left: left + 'px'
+	 	});
+	}
 
 	Spotlight.prototype.showImage = function(numberImage){
 	    this.keyOn=true;
 	    var self = this;// pass through closure
 	    var $image = $('#show'); //stock l'id $('#Show')
 	    var preloader = new Image();// use object image for grab image size
-		   	  
+
 	   	var windowWidth = $(window).width() , //stock window dimention
 	   		windowHeight  = $(window).height(),
 	   		imageWidth,
@@ -120,21 +133,21 @@
 	   		ratio,
 	   		size = 0.95;
 
-   		$('#show').hide();
+   	$('#show').hide();
 		$('#rightButton').hide();
 		$('#leftButton').hide();
-		$('#labelData').hide();	
-	   
+		$('#labelData').hide();
+
 	    preloader.onload = function(){ //onload function
 	    	$image.attr('src',self.album.linksList[numberImage].link); // change content on $('#show'), display
-			
+
 			//fit image on screen method
 			imageHeight = preloader.height
 			imageWidth = preloader.width
 
-					 	
+
 		 	if(preloader.width > windowWidth){
-	 			
+
 	 			if(preloader.height > windowHeight){
 	 				ratio = windowHeight / preloader.height;
 	 				imageHeight = preloader.height * ratio;
@@ -154,16 +167,16 @@
 	 		}else{
  				ratio = windowHeight / preloader.height;
  				imageHeight = windowHeight;
- 				imageWidth = preloader.width * ratio;	
+ 				imageWidth = preloader.width * ratio;
 	 			}
 
 	 		$image.height(imageHeight * size);
 	 		$image.width(imageWidth * size);
-	 		
+			self.scroll();
 	 		self.animation(imageWidth * size, imageHeight * size);
-	 		self.dataInfo(numberImage);	
+	 		self.dataInfo(numberImage);
 	 	}
-		preloader.src = this.album.linksList[numberImage].link;		
+		preloader.src = this.album.linksList[numberImage].link;
 	};
 
 	Spotlight.prototype.dataInfo =function(numberImage){
@@ -171,7 +184,7 @@
  		$('#spotlight').find(('#number')).text("Image count : "+(numberImage+1)+" of "+this.album.linksList.length);
 	};
 
-	
+
 	Spotlight.prototype.responsive = function(){
 		var self = this;
 		$( window ).resize(function(){
@@ -182,20 +195,20 @@
 
 	Spotlight.prototype.animation = function(imageWidth, imageHeight){
 	    var self = this;
-	    	
+
 
 	    var oldWidth  = $('#container').outerWidth(),
 	   		oldHeight = $('#container').outerHeight(),
 	    	newWidth  = imageWidth
 	    	newHeight = imageHeight
-	
+
 	    function postResize() {
 	    	$('#spotlight').find('#leftButton').height(newHeight);
 	    	$('#spotlight').find('#rightButton').height(newHeight);
 	    	$('#show').fadeIn('fast','swing');
 	    	$('#rightButton').fadeIn('slow','swing');
 			$('#leftButton').fadeIn('slow','swing');
-			$('#labelData').slideDown("slow",'swing'); 
+			$('#labelData').slideDown("slow",'swing');
 	    	}
 
 	    if (oldWidth !== newWidth || oldHeight !== newHeight) {
@@ -205,7 +218,7 @@
 	      	}, 120 , 'swing', function() {
 	        postResize();
 	      });
-    
+
 	    } else {
 	      postResize();
 	    }
@@ -215,60 +228,60 @@
 			var KEYCODE_ESC        = 27,
 				KEYCODE_LEFTARROW  = 37,
 				KEYCODE_RIGHTARROW = 39;
-			
-			if (input == "rightButton" || input == KEYCODE_RIGHTARROW){			
+
+			if (input == "rightButton" || input == KEYCODE_RIGHTARROW){
 				if(this.album.numberImage >= this.album.linksList.length -1){
 					this.album.numberImage = 0;
 					console.log("numberImage : "+this.album.numberImage+" album : "+this.album.linksList.length);
 				}else{
-					this.album.numberImage++; 
+					this.album.numberImage++;
 					console.log("numberImage : "+this.album.numberImage+" album : "+this.album.linksList.length);
 				}
 				this.sizeContainer();
 				return this.showImage(this.album.numberImage);
 
 			}
-			
-			else if (input == "leftButton" || input == KEYCODE_LEFTARROW){							
+
+			else if (input == "leftButton" || input == KEYCODE_LEFTARROW){
 				if(this.album.numberImage -1 < 0){
 					this.album.numberImage = this.album.linksList.length -1;
 					console.log("numberImage : "+this.album.numberImage+" album : "+this.album.linksList.length);
 				}else{
-					this.album.numberImage--; 
+					this.album.numberImage--;
 					console.log("numberImage : "+this.album.numberImage+" album : "+this.album.linksList.length);
 				}
 				this.sizeContainer();
-				return this.showImage(this.album.numberImage);	
+				return this.showImage(this.album.numberImage);
 			}
 
 			if(input == "closeButton" || input == KEYCODE_ESC){
-				this.end();	
+				this.end();
 			}
   	}
 
   	Spotlight.prototype.click = function() {
   		var self = this;
-  		var next = $('#spotlight').find('#rightButton'),		
+  		var next = $('#spotlight').find('#rightButton'),
 			prev = $('#spotlight').find('#leftButton'),
-			close = $('#spotlight').find('#closeButton');	
-		
+			close = $('#spotlight').find('#closeButton');
+
 		$('#spotlight').on('click','div[id = rightButton],div[id = leftButton],div[id = closeButton]',function(event){
 		var input = ($(event.currentTarget)).attr('id')
-		
+
 		self.navlogic(input);
 		});
   	}
 
   	Spotlight.prototype.keyboard = function(){
-  		
+
   		var	self = this; // pass through closure
-		
+
 			$('body').keydown(function(event){
 
 				var keycode = event.keyCode;
 				if(self.keyOn){
 				console.log(keycode);
-				self.navlogic(keycode);	
+				self.navlogic(keycode);
 				}else{console.log("keyboard off")}
 			});
   	}
@@ -280,6 +293,7 @@
 			console.log('tiggered')
 	    	$('#overlay').fadeOut('slow');
 	    	$spotlight.fadeOut('slow');
+				$('body').removeClass('noscroll');
 
 	};
 
